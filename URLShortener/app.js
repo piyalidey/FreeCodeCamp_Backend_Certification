b17c3fb89f18e84228c2f571475d9871e488c371
Piyali Dey <pdey@ncsu.edu>
2017-03-23 20:41:28 -0700

    Final Modifications

--- URLShortener/app.js
+++ URLShortener/app.js
@@ -11,12 +11,6 @@ var MongoClient = mongodb.MongoClient
 // removes underscores and dashes from possible characterlist
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 
-
-/* GET home page. */
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 // given a URL, create a shortcode and enter into the database
 app.get('/new/:url(*)', function (req, res, next) {
   MongoClient.connect(mLab, function(err,db) {
@@ -30,10 +24,12 @@ app.get('/new/:url(*)', function (req, res, next) {
         var local = req.get('host'); + "/";
         // function that imports a link to the db and returns shortLink
        var newLink = function (db, callback) {
+            // check for duplicates
             collection.findOne({ "url": params }, { short: 1, _id: 0 }, function (err, doc) {
                 if (doc != null) {
                     res.json({ original_url: params, short_url: local + doc.short });
             } else {
+                //new entry
                 if (validUrl.isUri(params)) {
                      // if URL is valid, do this
                     var shortCode = shortid.generate();
--- URLShortener/config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
- 
-config.db = {};
- 
-config.db.host = 'piyalidey:123456@ds139360.mlab.com:39360';
-config.db.name = 'url-short-fcc';
- 
-module.exports = config;
